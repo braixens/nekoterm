b5783311522124b21e0d753c6b75eb2b5db0a554
braixens <sylveons@mail.com>
2025-12-10 16:20:37 -0500

    uhhh kinda got a display i can work with for now

---
+++ src/blackjack.rs
@@ -0,0 +1,274 @@
+use rand::Rng;
+use std::fmt;
+use std::thread;
+#[derive(Debug, Clone, Copy)]
+pub enum Suit {
+    Spades,
+    Hearts,
+    Diamonds,
+    Clubs,
+}
+
+impl fmt::Display for Suit {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let suit = match self {
+            Suit::Clubs => "♣",
+            Suit::Diamonds => "♦",
+            Suit::Hearts => "♥",
+            Suit::Spades => "♠",
+        };
+        write!(f, "{}", suit)
+    }
+}
+
+#[derive(Debug)]
+pub enum CardType{
+    Numbered(u8),
+    Face(Face),
+    Ace,
+}
+
+impl fmt::Display for CardType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CardType::Numbered(number) => write!(f, "({})", number),
+            CardType::Face(face) => write!(f, "({})", face),
+            CardType::Ace => write!(f, "(A)"),
+
+        }
+    }
+}
+#[derive(Debug)]
+pub enum Face {
+    King,
+    Queen,
+    Jack,
+}
+
+impl fmt::Display for Face {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let face = match self {
+            Face::King => "King",
+            Face::Queen => "Queen",
+            Face::Jack => "Jack",
+        };
+        write!(f, "{}", face)
+    }
+}
+
+#[derive(Debug)]
+pub struct Card {
+    card_type: CardType,
+    suit: Suit,
+}
+impl Card {
+    pub fn card_type(&self) -> &CardType {
+        &self.card_type
+    }
+
+    pub fn suit(&self) -> &Suit {
+        &self.suit
+    }
+}
+
+#[derive(Debug)]
+pub struct Deck {
+    cards: Vec<Card>
+}
+
+impl Deck {
+
+    pub fn cards(&self) -> &Vec<Card> {
+        &self.cards
+    }
+    pub fn new() -> Self {
+        let mut cards = Vec::new();
+
+        for suit in &[Suit::Spades, Suit::Hearts, Suit::Diamonds, Suit::Clubs] {
+            for number in 2..=10 {
+                cards.push(Card {
+                    card_type: CardType::Numbered(number),
+                    suit: *suit,
+                })
+            }
+        }
+
+        for suit in &[Suit::Spades, Suit::Hearts, Suit:: Diamonds, Suit::Clubs]{
+            cards.push(Card{
+                card_type: CardType::Face(Face::King),
+                suit: *suit,
+            });
+            cards.push(Card{
+                card_type: CardType::Face(Face::Queen),
+                suit: *suit,
+            });
+            cards.push(Card{
+                card_type: CardType::Face(Face::Jack),
+                suit: *suit,
+            });
+        }
+
+        for suit in &[Suit::Spades, Suit::Hearts, Suit:: Diamonds, Suit::Clubs] {
+            cards.push(Card{
+                card_type: CardType::Ace,
+                suit: *suit,
+            })
+        }
+        Deck { cards }
+    }
+
+    pub fn draw(&mut self) -> Option<Card>{
+        if !self.cards.is_empty() {
+            self.cards.pop()
+        } else {
+            println!("There are no cards left...");
+            None
+        }
+    }
+
+    pub fn shuffle(&mut self) {
+        for i in (1..self.cards.len()).rev(){
+            let mut rng = rand::rng();
+            let j = rng.random_range(0..=i);
+            self.cards.swap(i, j);
+        }
+
+    }
+}
+
+#[derive(Debug)]
+pub struct Player {
+    name: String,
+    hand: Vec<Card>,
+    hand_value: i32,
+}
+impl Player {
+    pub fn new(name: String) -> Self{
+        Player {
+            name,
+            hand: Vec::new(),
+            hand_value: 0,
+        }
+    }
+
+    pub fn draw_card(&mut self, deck: &mut Deck){
+        if let Some(card) = deck.draw(){
+            self.hand.push(card);
+            self.calculate_hand();
+        }
+    }
+
+    pub fn calculate_hand(&mut self) -> i32 {
+        self.hand_value = 0;
+        let mut ace_count = 0;
+        for card in &self.hand {
+            match card.card_type {
+                CardType::Numbered(num) => self.hand_value += num as i32,
+                CardType::Face(_) => self.hand_value += 10,
+                CardType::Ace => {
+                    self.hand_value += 11;
+                    ace_count += 1;
+                }
+            }
+        }
+        while self.hand_value > 21 && ace_count > 0 {
+            self.hand_value -= 10;
+            ace_count -= 1
+        }
+        self.hand_value
+    }
+
+    pub fn is_busted(&self) -> bool {
+        self.hand_value > 21
+    }
+
+    pub fn has_blackjack(&self) -> bool {
+        self.hand_value == 21 && self.hand.len() == 2
+    }
+
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+    pub fn hand(&self) -> &Vec<Card> {
+        &self.hand
+    }
+    pub fn hand_value(&self) -> i32 {
+        self.hand_value
+    }
+}
+
+#[derive(Debug)]
+pub struct Game {
+    dealer: Player,
+    players: Vec<Player>,
+    deck: Deck
+}
+
+impl Game {
+    pub fn new() -> Self {
+        let mut deck = Deck::new();
+        deck.shuffle();
+        Game {
+            dealer: Player::new("Dealer".to_string()),
+            players: Vec::new(),
+            deck,
+        }
+    }
+    pub fn single_player(name: String) -> Self {
+        let mut deck = Deck::new();
+        deck.shuffle();
+        Self {
+            dealer: Player::new("Dealer".to_string()),
+            players: vec![
+                Player::new(name),
+            ],
+            deck,
+        }
+    }
+
+    pub fn add_player(&mut self, name: String){
+        let player = Player::new(name);
+        self.players.push(player);
+    }
+
+    pub fn start(&mut self) {
+        self.deck.shuffle();
+        for player in &mut self.players{
+            while player.hand.len() < 2{
+                player.draw_card(&mut self.deck)
+            }
+        }
+    }
+
+    pub fn player_draw_card(&mut self, player_index: usize) {
+        self.players[player_index].draw_card(&mut self.deck);
+        self.players[player_index].hand_value = self.players[player_index].calculate_hand();
+    }
+
+    pub fn dealer_turn(&mut self) {
+        while self.dealer.hand_value < 17 && !self.dealer.has_blackjack() {
+            self.dealer.draw_card(&mut self.deck);
+            self.dealer.calculate_hand();
+        }
+    }
+
+    pub fn players(&self) -> &Vec<Player> {
+        &self.players
+    }
+
+    pub fn deck(&self) -> &Deck {
+        &self.deck
+    }
+
+    pub fn dealer(&self) -> &Player {
+        &self.dealer
+    }
+    pub fn player_turn () {
+        todo!()
+    }
+}
+impl Default for Game {
+    fn default() -> Self {
+        Self::single_player("Dime".to_string())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,128 @@
-fn main() {
-    println!("Hello, world!");
+
+
+mod blackjack;
+use crate::blackjack::{Game, Deck, Card};
+use std::time::Duration;
+use std::thread;
+use std::io;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = App::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    game: Game,
+    exit: bool,
+}
+
+impl App {
+    // runs application until quit
+    pub fn run (&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Char('h') => self.hit(),
+            KeyCode::Char('s') => self.stand(),
+            _ => {}
+        }
+    }
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn hit(&mut self) {
+        self.game.player_draw_card(0);
+    }
+
+    fn stand(&mut self) {
+        self.game.dealer_turn();
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Blackjack ".bold());
+        let instructions = Line::from(vec![
+            " | Hit ".into(),
+            " <H> ".blue().bold(),
+            "| Stand ".into(),
+            " <S> ".blue().bold(),
+            "| Quit ".into(),
+            "<Q> ".blue().bold(),
+            "| ".into(),
+        ]);
+
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let dealer = self.game.dealer();
+
+        let player = match self.game.players().get(0) {
+            Some(player) => player,
+            None => {
+                return;
+            }
+        };
+
+        let card_strings: Vec<String> = player.hand().iter().map(|card| format!("{}{}", card.card_type(), card.suit())).collect();
+        let cards_display = card_strings.join(", ");
+        let hand_info = vec![Line::from(vec!["Hand ".into(), cards_display.into()]),
+                             Line::from(vec!["Value ".into(), self.game.players()[0].hand_value().to_string().yellow()])];
+        let hand_text = Text::from(hand_info);
+        let dealer_cards: Vec<String> = dealer.hand().iter().map(|card| format!("{}{}", card.card_type(), card.suit())).collect();
+        let dealer_display = dealer_cards.join(", ");
+        let dealer_info = vec![Line::from(vec!["Dealer ".into(), dealer_display.into()]),
+                               Line::from(vec!["Value ".into(), self.game.dealer().hand_value().to_string().yellow()])];
+        let dealer_text = Text::from(dealer_info);
+
+        match self.game.players()[0].is_busted() {
+            false => {
+                Paragraph::new(format!("{} \n {}", hand_text, dealer_text))
+                    .centered()
+                    .block(block)
+                    .render(area, buf);
+            },
+            true => {
+                Paragraph::new("YOU LOSE")
+                    .centered()
+                    .block(block)
+                    .render(area, buf);
+            }
+        }
+    }
 }
